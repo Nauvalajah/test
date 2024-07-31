cfffb1cf35ac943a859e95fc7e5eeec0f90f404b
Nauvalajah <nauval_hisyam_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:58:37 +0700

    Add files via upload
    Ini perkenalan saya menggunakan coding pertama saya  nama saya Nauval Faiz Luthf Hisyam dan

---
+++ perkenalan.java
@@ -0,0 +1,10 @@
+public class perkenalan {
+public static void main(String[] args) {
+    System.out.println("Nama: Nauval Faiz Luthf Hisyam");
+    System.out.println("alamat: Dsn. Umbulrejo RT 2 RW 3 Desa Bagorejo Kecamatan Srono Kab. Banyuwangi");
+    System.out.println("SMP asal : SMPN 1 Srono");
+    System.out.println("No hp aing: 085257270625");
+System.out.println("hobi saya voly ball");
+}
+    
+}
